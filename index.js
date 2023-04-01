ee38a4dd041ac9ec90477e114abc708fd8a2e324
Pragati Patel <pragatinpatel@gmail.com>
2023-04-01 09:42:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(collection, name) {
+    const matchingDriver = [];
+    for (let driver of collection) {
+      if (driver === name || driver.toUpperCase() == name.toUpperCase()) {
+        matchingDriver.push(driver)
+      }
+    }
+    return matchingDriver;
+}
+  
+function fuzzyMatch(collection, name) {
+    const matchingDriver = [];
+    for (let driver of collection) {
+      if (driver[0] === name[0] && driver[1] === name[1]) {
+        matchingDriver.push(driver);
+      }
+    }
+    return matchingDriver;
+}
+  
+function matchName(collection, name) {
+    const matchingDrivers = [];
+    for (let driver of collection) {
+      if (driver.name ===  name) {
+        matchingDrivers.push(driver);
+      }
+    }
+    return matchingDrivers;
+}
